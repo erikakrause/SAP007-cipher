c4d0f8e604311baf24b84838f9e076bd8fb0825a
Erika Moreno <erika.kmoreno@gmail.com>
2022-01-19 09:22:42 -0300

    correcao declaracao funcao

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import cipher from './cipher.js';
 
 document.getElementById('btnEncode').addEventListener("click", cifrar);
-document.getElementById("btnDecode").addEventListener("click", decifrar)
+document.getElementById("btnDecode").addEventListener("click", decifrar);
 
 
 function cifrar () {
   let texto = document.getElementById("txtEncode").value;
-
-  console.log(texto)
   
+}
+
+  console.log(texto);  
