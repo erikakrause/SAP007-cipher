0b1e05ebcf9f885abb9ba4eaca0426a9ccc8fce7
Erika Moreno <erika.kmoreno@gmail.com>
2022-01-25 15:37:24 -0300

    funcao encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,17 @@
 const cipher = {
-  encode: encode,
-  decode: decode
-};
+  encode(offset, texto) {
+    let resultadoEncode = "";
+    let letras = texto.length
+    for (let i = 0; i < letras; i++) {
+      let codigoAsc = texto.charCodeAt(i)
+      let codigoLetra = 65
+      let valorEncode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra;
+      resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorEncode));
+    
+    }
+    
+  return resultadoEncode; 
+  }
+}
 
 export default cipher;
