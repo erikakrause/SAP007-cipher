6ba7fbb79365de42b2d168da01385ce51388fc53
Erika Moreno <erika.kmoreno@gmail.com>
2022-01-26 17:47:36 -0300

    correção função encode

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-//import cipher from './cipher.js';
+import cipher from './cipher.js';
 
 document.getElementById('btnEncode').addEventListener("click", cifrar);
+//document.getElementById('btnDecode').addEventListener("click", decifrar);
+document.getElementById("txtResult");
 
 function cifrar () {
-  let texto = document.getElementById("txtEncode").value;
+  let texto = document.getElementById("message").value;
+  let offset = Number(document.getElementById("offset").value);
+      document.getElementById("txtResult").value=cipher.encode(offset, texto);
+    } 
   
   console.log(texto); 
-}
+
+/*
+function decifrar () {
+  let texto = document.getElementById("message").value;
+  let offset = Number(document.getElementById("offset").value);
+  
+      document.getElementById("txtResult").value=cipher.encode(offset, texto);
+    }
+  
+  console.log(texto); */
+
+
