f1330bd68a81ed13743608d6aadc95f1d174dcb4
Erika Moreno <erika.kmoreno@gmail.com>
2022-01-26 22:35:26 -0300

    declarado cipher decode

--- src/cipher.js
+++ src/cipher.js
@@ -7,10 +7,23 @@ const cipher = {
       let codigoLetra = 65
       let valorEncode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra;
       resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorEncode));
-    
     }
     
   return resultadoEncode; 
+  },
+
+  decode(offset, texto) {
+    let resultadoDecode = "";
+    let letras = texto.length
+    for (let i = 0; i < letras; i++) {
+      let codigoAsc = texto.charCodeAt(i)
+      let codigoLetra = 90
+      let valorDecode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra;
+      resultadoDecode = resultadoDecode.concat(String.fromCharCode(valorDecode));
+    
+    }
+    
+  return resultadoDecode; 
   }
 }
 
