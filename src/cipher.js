dd93c0a6316797c7f75393174869cb90932ffa8b
Erika Moreno <erika.kmoreno@gmail.com>
2022-01-27 19:10:36 -0300

    correcao erro de espaçamento string

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,42 @@
 const cipher = {
+  // calculo para cifrar
   encode(offset, texto) {
     let resultadoEncode = "";
     let letras = texto.length
     for (let i = 0; i < letras; i++) {
       let codigoAsc = texto.charCodeAt(i)
       let codigoLetra = 65
-      let valorEncode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra;
-      resultadoEncode = resultadoEncode.concat(String.fromCharCode(valorEncode));
+      if (codigoAsc >= 65 && codigoAsc <= 90) {
+        let valorEncode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra;
+        resultadoEncode = resultadoEncode + String.fromCharCode(valorEncode);
+      } else { 
+        resultadoEncode = resultadoEncode + String.fromCharCode(codigoAsc);
+      }
+    
     }
     
   return resultadoEncode; 
   },
 
   decode(offset, texto) {
+    //calculo para decifrar
     let resultadoDecode = "";
     let letras = texto.length
     for (let i = 0; i < letras; i++) {
       let codigoAsc = texto.charCodeAt(i)
       let codigoLetra = 90
-      let valorDecode = ((codigoAsc - codigoLetra - offset)) % 26 + codigoLetra;
-      resultadoDecode = resultadoDecode.concat(String.fromCharCode(valorDecode));
-    
-    }
-    
+        if (codigoAsc <= 90 && codigoAsc >= 65) { 
+          let valorDecode = ((codigoAsc - codigoLetra - offset)) % 26 + codigoLetra;
+          resultadoDecode = resultadoDecode + String.fromCharCode(valorDecode);
+        } else { 
+          resultadoDecode = resultadoDecode + String.fromCharCode(codigoAsc);
+        }
+
+        }
+      
   return resultadoDecode; 
-  }
-}
+      }  
+    }
 
 export default cipher;
+
