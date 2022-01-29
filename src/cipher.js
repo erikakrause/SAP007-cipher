7de4cc6f19634e0057d27191bb8557fe5816b644
Erika Moreno <erika.kmoreno@gmail.com>
2022-01-29 13:46:05 -0300

    comentarios

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,14 @@
 const cipher = {
-  // calculo para cifrar
+  
   encode(offset, texto) {
-    let resultadoEncode = "";
+    let resultadoEncode = ""; 
     let letras = texto.length
     for (let i = 0; i < letras; i++) {
-      let codigoAsc = texto.charCodeAt(i) 
-      let codigoLetra = 65 
-      if (codigoAsc >= 65 && codigoAsc <= 90) {
-        let valorEncode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra;
-        resultadoEncode = resultadoEncode + String.fromCharCode(valorEncode);
+      let codigoAsc = texto.charCodeAt(i) //transforma o alfabeto padrao para o ASC
+      let codigoLetra = 65 //numero da chave inicial do alfabero ASC
+      if (codigoAsc >= 65 && codigoAsc <= 90) { // ler apenas as letras 
+        let valorEncode = ((codigoAsc + codigoLetra + offset)) % 26 + codigoLetra; //calculo do deslocamento para cifrar
+        resultadoEncode = resultadoEncode + String.fromCharCode(valorEncode); //retorna uma string criada ao usar uma sequência específica de valores Unicode
       } else { 
         resultadoEncode = resultadoEncode + String.fromCharCode(codigoAsc);
       }
@@ -19,14 +19,14 @@ const cipher = {
   },
 
   decode(offset, texto) {
-    //calculo para decifrar
+
     let resultadoDecode = "";
     let letras = texto.length
     for (let i = 0; i < letras; i++) {
       let codigoAsc = texto.charCodeAt(i)
       let codigoLetra = 90
         if (codigoAsc <= 90 && codigoAsc >= 65) { 
-          let valorDecode = ((codigoAsc - codigoLetra - offset)) % 26 + codigoLetra;
+          let valorDecode = ((codigoAsc - codigoLetra - offset)) % 26 + codigoLetra; //calculo p/ decifrar
           resultadoDecode = resultadoDecode + String.fromCharCode(valorDecode);
         } else { 
           resultadoDecode = resultadoDecode + String.fromCharCode(codigoAsc);
